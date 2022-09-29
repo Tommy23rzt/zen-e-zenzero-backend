b9d793837a15cb319e5e6b87f1a2ac3b6a37b73e
Tommy23gun <tommaso.rizzardi@gmail.com>
2022-09-29 17:30:27 +0200

    first deploy

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Listening at port 5000");
 });
